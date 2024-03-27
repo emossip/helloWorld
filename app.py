339ac39e7de7312407dd52f87410f28b19cfb2e7
emossip <emossip@terpmail.umd.edu>
2024-03-27 17:37:52 -0400

    Uploading contact.html and favorite-course.html files along with their base.hmtl file

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 
 @app.route('/')
@@ -9,11 +9,27 @@ def hello_world():  # put application's code here
 
 @app.route('/hello')
 def hello():
-    return render_template('hello.html')
+    return render_template('/hello.html')
 
 @app.route('/about-css')
 def about_css():
     return render_template('about-css.html')
+
+@app.route('/contact', methods=['GET','POST'])
+def contact():
+    if request.method == 'POST':
+        return render_template('contact.html',form_submitted=True)
+    else:
+        return render_template('contact.html')
+
+@app.route('/favorite-course')
+def favorite_course():
+    print('You entered your favorite course as:' + str(request.args.get('subject')) + str(request.args.get('course_number')))
+    return render_template('favorite-course.html')
+
+@app.route('/base')
+def base():
+    return render_template('base.html')
 
 if __name__ == '__main__':
     app.run()
